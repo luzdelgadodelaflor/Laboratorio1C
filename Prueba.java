3d901e949fd22fb1a6e96d7af8cb79d2a429f3c9
Angela Zapata <fran.angelazm@gmail.com>
2025-04-28 23:52:04 -0500

    Inicio de la calculadora

---
+++ Prueba.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+import java.util.Scanner;
+public class Prueba{
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Calculadora");
+        }
+    }
+}
