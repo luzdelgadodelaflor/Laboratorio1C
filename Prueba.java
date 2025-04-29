cbcb94cfd3e9ddfc4885e27f72515827fdc8de94
Angela Zapata <fran.angelazm@gmail.com>
2025-04-29 00:04:58 -0500

    Se implemento las operaciones de suma, resta, multiplicacion y division

--- Prueba.java
+++ Prueba.java
@@ -21,6 +21,23 @@ public class Prueba{
             int opcion = scanner.nextInt();
             
             double resultado;
+            switch (opcion) {
+                case 1 -> resultado = num1 + num2;
+                case 2 -> resultado = num1 - num2;
+                case 3 -> resultado = num1 * num2;
+                case 4 -> {
+                    if (num2 == 0) {
+                        System.out.println("Error: No se puede dividir entre cero.");
+                        return;
+                    }
+                    resultado = num1 / num2;
+                }
+                default -> {
+                    System.out.println("Opción no válida.");
+                    return;
+                }
+            }
+            System.out.println("El resultado es: " + resultado);
         }
     }
 }
