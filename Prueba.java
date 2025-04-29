653999f379d9034ebac93dd567f8e6404041d112
Angela Zapata <fran.angelazm@gmail.com>
2025-04-29 00:02:54 -0500

    Permite al usuario ingresar 2 numeros y seleccionar la operacion matematica

--- Prueba.java
+++ Prueba.java
@@ -7,6 +7,20 @@ public class Prueba{
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Calculadora");
+            System.out.println("Ingrese el primer número: ");
+            double num1 = scanner.nextDouble();
+            
+            System.out.println("Ingrese el segundo número: ");
+            double num2 = scanner.nextDouble();
+            
+            System.out.println("Seleccione una operación: ");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            int opcion = scanner.nextInt();
+            
+            double resultado;
         }
     }
 }
